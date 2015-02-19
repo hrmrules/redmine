bfc1cad1496704db700e2155a81682d9df1c972a
Hans Mahajan <hans@mahajan.it>
2015-02-19 12:40:35 +0100

    Change CLOB to varchar2(4000) on changes (Oracle)

--- db/migrate/035_create_changes.rb
+++ db/migrate/035_create_changes.rb
@@ -3,8 +3,8 @@ class CreateChanges < ActiveRecord::Migration
     create_table :changes do |t|
       t.column :changeset_id, :integer, :null => false
       t.column :action, :string,  :limit => 1, :default => "", :null => true
-      t.column :path, :text, :default => "", :null => true
-      t.column :from_path, :text
+      t.column :path, :string, :limit => 4000, :default => "", :null => true
+      t.column :from_path, :string, :limit => 4000
       t.column :from_revision, :integer
     end
     add_index :changes, [:changeset_id], :name => :changesets_changeset_id
